4d21f394b399357f9eca28ec237457a845bbfec2
Jonathan <jon8921@hotmail.com>
2023-08-01 15:54:17 -0500

    correct

---
+++ index.js
@@ -0,0 +1,36 @@
+// index.js
+
+function add(a, b) {
+    return a + b;
+  }
+  
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  function multiply(a, b) {
+    return a * b;
+  }
+  
+  function divide(a, b) {
+    return a / b;
+  }
+  
+  function increment(n) {
+    return n + 1;
+  }
+  
+  function decrement(n) {
+    return n - 1;
+  }
+  
+  // Define utility functions
+  
+  function makeInt(n) {
+    return parseInt(n, 10);
+  }
+  
+  function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
